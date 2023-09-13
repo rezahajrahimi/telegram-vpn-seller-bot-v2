23dcc7d9769c4b821ef94d9855d0b0071e3177e7
Reza HajRahimi <rezahajrahimi@yahoo.com>
2023-09-13 19:28:36 +0330

    add sumbit to payment gateway

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('buy/{price}/{invoiceID}',function($price,$invoiceID){
+    return view('shop',['price'=>$price , 'invoiceID'=>$invoiceID]);
+});
+
+// Route::get('order','siteController@order');
+// Route::post('shop','siteController@add_order');
